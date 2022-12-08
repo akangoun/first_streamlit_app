f1440a99c56cd74eb01de05b5d5c325fcd3175b8
akangoun <61240598+akangoun@users.noreply.github.com>
2022-12-08 14:11:54 -0500

    Update streamlit_app.py
    error fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Bluberry Oatmeal')
-Stremlit.text('Kale, spinach & Rocket smoothies')
+streamlit.text('Kale, spinach & Rocket smoothies')
 streamlit.text('hard boiled free range egg')
