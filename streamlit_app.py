8fb5dfa6d3190fd533b0f9f3be2d5976319d0147
akangoun <61240598+akangoun@users.noreply.github.com>
2022-12-08 15:30:32 -0500

    Update streamlit_app.py
    added csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ streamlit.text('😎😎Omega 3 & Bluberry Oatmeal')
 streamlit.text('Kale, spinach & Rocket smoothies✔')
 streamlit.text('hard boiled free range egg')
 streamlit.header('🦖🦖🤞🤞Build your own smothie')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
