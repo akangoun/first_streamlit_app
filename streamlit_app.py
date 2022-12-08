86707178de64a07a32d01520a843fa763af5c0cb
akangoun <61240598+akangoun@users.noreply.github.com>
2022-12-08 14:10:55 -0500

    Update streamlit_app.py
    Added more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluberry Oatmeal')
+Stremlit.text('Kale, spinach & Rocket smoothies')
+streamlit.text('hard boiled free range egg')
